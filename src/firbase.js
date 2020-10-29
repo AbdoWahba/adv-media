d6d37aefefbc20628040d28671fbfb35c91f2248
AbdoWahba <abdo.tw20154@gmail.com>
2020-10-29 19:50:50 +0200

    adding firebase and firestone configuration to app enable sign in using google

---
+++ src/firbase.js
@@ -0,0 +1,20 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase';
+const firebaseConfig = {
+  apiKey: 'AIzaSyCeQ_y2L-OglIYP3c89l9Q0oYx_a7SNyhQ',
+  authDomain: 'advmedia-atw.firebaseapp.com',
+  databaseURL: 'https://advmedia-atw.firebaseio.com',
+  projectId: 'advmedia-atw',
+  storageBucket: 'advmedia-atw.appspot.com',
+  messagingSenderId: '757747673095',
+  appId: '1:757747673095:web:5a3a55405d3675ef1962a2',
+  measurementId: 'G-L8GZZJYVLC',
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const db = firebaseApp.firestore();
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+export { auth, provider };
+export default db;
