1cf192fd403c54225ac1904fc4d566f04a570b5e
AbdoWahba <abdo.tw20154@gmail.com>
2020-10-29 22:50:34 +0200

    add routing to app

--- src/App.js
+++ src/App.js
@@ -2,13 +2,22 @@ import React from 'react';
 import './App.css';
 import Header from './Components/Header';
 import Feed from './Components/Feed';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import CreatePost from './Components/CreatePost';
+import SignIn from './Components/SignIn';
 
 function App() {
   return (
-    <div className='App'>
-      <Header />
-      <Feed />
-    </div>
+    <BrowserRouter>
+      <Switch>
+        <div className='App'>
+          <Header />
+          <Route exact path='/' component={Feed} />
+          <Route path='/create' component={CreatePost} />
+          <Route path='/signin' component={SignIn} />
+        </div>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
