9fb0b9ca94ab799b3a7f7fec6555a300036abe9f
AbdoWahba <abdo.tw20154@gmail.com>
2020-10-30 07:20:38 +0200

    featch the user using firbase auth and google login

--- src/Components/SignIn.js
+++ src/Components/SignIn.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import { auth, provider } from '../firbase';
 import { Button } from '@material-ui/core';
+import { useStateValue } from '../store/Provider';
+import { actionTypes } from '../store/reducers';
 
 import './SignIn.css';
 
 function SignIn() {
+  const [state, dispatch] = useStateValue();
   const signIn = () => {
     auth
       .signInWithPopup(provider)
-      .then((res) => console.log('res: ', res))
+      .then(({ user }) => {
+        console.log('res: ', user);
+        localStorage.setItem('user', JSON.stringify(user));
+        dispatch({ type: actionTypes.SET_USER, user: user });
+      })
       .catch((err) => console.log('err: ', err));
   };
   return (
